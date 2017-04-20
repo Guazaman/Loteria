feb986af914a1f9c60d055caeb47acf89f73d8e7
César Robles <cesar.robles@oracle.com>
2017-04-19 20:09:26 -0500

    Initial webserver and frontend structure

---
+++ WebpageServer/client/js/app.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Raptor on 13/06/16.
+ */
+(function() {
+
+    let equipo6 = angular.module('equipo6', [
+        'ngRoute',
+        'equipo6Controllers',
+        'ngSanitize'
+    ]);
+
+    equipo6.value('header', {
+        brand: 'todos',
+        model: 'todos',
+        type: 'todos'
+    });
+
+    equipo6.value('comparar', {
+        items: [0, 0, 0],
+        numero: 0
+    });
+
+    equipo6.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+        $routeProvider.
+        when('/', {
+            templateUrl: 'views/inicio.html',
+            controller: 'InicioController'
+        }).
+        otherwise('/', {
+            templateUrl: 'views/inicio.html'
+        });
+        $locationProvider.html5Mode(true);
+    }]);
+})();
---
+++ WebpageServer/client/js/controllers.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Raptor on 13/06/16.
+ */
+let equipo6Controllers = angular.module('equipo6Controllers', []);
+equipo6Controllers.controller('InicioController', ['$scope',  'header', function($scope, header){
+
+}]);
