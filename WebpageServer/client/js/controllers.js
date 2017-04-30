943106671d3a6132050472609ef978983c1633e5
César Robles <chechare_94@hotmail.com>
2017-04-30 16:58:04 -0500

    Profile view

--- WebpageServer/client/js/controllers.js
+++ WebpageServer/client/js/controllers.js
@@ -1,7 +1,12 @@
 /**
  * Created by Raptor on 13/06/16.
  */
-let equipo6Controllers = angular.module('equipo6Controllers', []);
-equipo6Controllers.controller('InicioController', ['$scope',  'header', function($scope, header){
+let loteriaGameControllers = angular.module('loteriaGameControllers', []);
+
+loteriaGameControllers.controller('InicioController', ['$scope',  'header', function($scope, header){
+
+}]);
+
+loteriaGameControllers.controller('ProfileController', ['$scope',  'header', function($scope, header){
 
 }]);
