6a048ead4aff24b9fd9fa0958b1805e12ef97ec7
César Robles <chechare_94@hotmail.com>
2017-05-02 22:50:46 -0500

    Logout controller and header modifier

--- WebpageServer/client/js/controllers.js
+++ WebpageServer/client/js/controllers.js
@@ -87,7 +87,9 @@ loteriaGameControllers.controller('ProfileController', ['$scope', '$http', '$roo
     $http(req).then(function successCallback(response) {
       // this callback will be called asynchronously
       // when the response is available
-      $window.location.href = '/'
+      $cookieStore.remove('id');
+      $cookieStore.remove('name');
+      $window.location.href = '/';
 
     }, function errorCallback(response) {
       // called asynchronously if an error occurs
@@ -182,12 +184,17 @@ loteriaGameControllers.controller('LoginController', ['$scope', '$http', '$rootS
       // called asynchronously if an error occurs
       // or server returns response with an error status.
       console.log(response);
+      $scope.loginError = true;
 
     });
   }
 }]);
 
-
+loteriaGameControllers.controller('LogoutController', ['$scope', '$http', '$rootScope', '$cookies', '$cookieStore', '$window', function ($scope, $http, $rootScope, $cookies, $cookieStore, $window){
+    $cookieStore.remove('id');
+    $cookieStore.remove('name');
+    $window.location.href = '/';
+}]);
 
 loteriaGameControllers.controller('RegisterController',  ['$scope', '$http', '$rootScope', '$cookies', '$cookieStore', '$window', function ($scope, $http, $rootScope, $cookies, $cookieStore, $window){
   $scope.registerController = function() {
