cc7810d0c7bac4a0954afde2886ed3e4772e97c6
kimbuca <kimbuca.rola@gmail.com>
2017-05-03 01:07:15 -0500

    small refactor

--- GameServer/controllers/res/usercards.js
+++ GameServer/controllers/res/usercards.js
@@ -1,1 +1,10 @@
-//deberian ir los arreglos
+exports.usercards = [
+["el gallo", "la campana", "la garza", "el borracho", "el mundo", "el soldado", "la bandera", "el violoncello", "la estrella", "el arbol", "la maceta", "la sandia", "la escalera", "el sol", "el barril", "el negrito"],
+["las jaras", "el musico", "la bandera", "el cantarito", "la botella", "el valiente", "el corazon", "el mundo", "la garza", "el pajaro", "el nopal", "el diablito", "el pino", "el catrin", "la luna", "el pescado"],
+["el soldado", "la corona", "la calavera", "el negrito", "el arpa", "la luna", "la garza", "la maceta", "la estrella", "la escalera", "el paraguas", "el melon", "la bota", "la muerte", "el camaron", "el alacran"],
+["la bandera", "el cotorro", "la sandia", "el barril", "la calavera", "el musico", "el mundo", "el gallo", "el cazo", "la rosa", "el cantarito", "el borracho", "el gorrito", "el pino", "la mano", "la rana"],
+["la campana", "el apache", "la bota", "el bandolon", "el arbol", "el borracho", "la escalera", "el diablito", "el gallo", "la araña", "el pescado", "la muerte", "el negrito", "la corona", "el pino", "el tambor"],
+["el corazon", "el diablito", "el cantarito", "e pajaro", "el negrito", "el apache", "el gallo", "el bandolon", "la estrella", "el cazo", "el melon", "el arpa", "el tambor", "la botella", "la corona", "el arbol"],
+["la estrella", "el paraguas", "el cotorro", "la palma", "el pescado", "la dama", "el bandolon", "el musico", "el venado", "la sandia", "el apache", "el barril", "la mano", "la araña", "el alacran", "el pajaro"],
+["el valiente", "la pera", "las jaras", "el cotorro", "la campana", "la sirena", "el arpa", "la estrella", "la rosa", "el pajaro", "el cazo", "la rana", "la luna", "la chalupa", "la botella", "el corazon"],
+["el melon", "la sandia", "la dama", "el venado", "la mano", "el corazon", "el nopal", "el diablito", "la garza", "el violoncello", "la rana", "el camaron", "el barril", "la chalupa"]];
